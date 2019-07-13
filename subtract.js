bd48e38f18a8134428bc7c2d6de06fe65d7de983
Pradeep Sethi <pradeep.2.sethi@gsk.com>
2019-07-13 23:30:21 +0100

    Revert "extra feature in subtract function"
    This reverts commit 6a506e51aa520451b7b164df7e450f2e64b66bc8.

--- subtract.js
+++ subtract.js
@@ -1,9 +1,4 @@
 // subtract.js// return subtraction of two numbers
-function subtract(a, b, bMINUSa) {
-    if (bMINUSa === true) {
-        return b - a;
-    }
-    else {
-        return a - b;
-    }
+function subtract(a, b) {
+    return a - b;
 };
